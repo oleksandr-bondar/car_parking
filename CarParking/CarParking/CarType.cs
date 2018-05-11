c68ffd5eb563ef8b7108e29fd73cf3ab69bde662
oleksandr-bondar <bondar.sashko@gmail.com>
2018-05-11 14:40:51 +0300

    Created class "CarType" (fixes #3)

---
+++ CarParking/CarParking/CarType.cs
@@ -0,0 +1,25 @@
+﻿namespace CarParking
+{
+    /// <summary>
+    /// Представляет тип машины.
+    /// </summary>
+    public enum CarType
+    {
+        /// <summary>
+        /// Легковая.
+        /// </summary>
+        Passenger = 1,
+        /// <summary>
+        /// Грузовая.
+        /// </summary>
+        Truck = 2,
+        /// <summary>
+        /// Автобус.
+        /// </summary>
+        Bus = 3,
+        /// <summary>
+        /// Мотоцикл.
+        /// </summary>
+        Motorcycle = 4
+    }
+}
